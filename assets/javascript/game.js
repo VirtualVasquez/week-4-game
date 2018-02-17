becda0504cf0206b446542e1ecdc8d0d16a29ccc
Melvin Vasquez <mxv647@med.miami.edu>
2018-02-17 00:38:57 -0500

    html + psuedocoding JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//GLOBAL VARIABLES
+//=====================================================
+
+// on load, random number generated between 19-120
+
+//on load, random number assigned from 1-12 to each crystal
+//for the duration of the game 
+
+//on click, crystals will add unknown assigned No. to score
+
+//if score=random number, player wins, +1 to win score
+
+//if score>random number, player loses, +1 to lose score
+
+//Win or Lose, new number generated, new values to cyrstals
+//score is set to zero
+
+//
