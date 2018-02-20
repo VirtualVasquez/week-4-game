fb6b05d3b974f283904be4072a56908b7d5d9fb5
Melvin Vasquez <mxv647@med.miami.edu>
2018-02-19 23:52:51 -0500

    JS working, fails to reset properly, needs CSS touchups

--- assets/javascript/experiment.js
+++ assets/javascript/experiment.js
@@ -1,10 +1,10 @@
 //GLOBAL VARIABLES
 //=====================================================
 
-var random = 0;
+var ranNum = 0;
 var wins = 0;
 var losses = 0;
-var score = 0;
+var score = '';
 var green = 0;
 var red = 0;
 var blue = 0;
@@ -12,13 +12,54 @@ var yellow = 0;
 
 //FUNCTIONS
 //====================================================================================
+function reset() 
+{
+	//RESET
+  	//=====================================================
+  		ranNum = 0;
+ 		score = '';
+ 		green = 0;
+ 		red = 0;
+ 		blue = 0;
+ 		yellow = 0;
+	// on load, random number generated between 19-120
+    	var ranNum = 19 + Math.floor(Math.random() * 100);
+        $('#ranNum').text(ranNum);
+        console.log(ranNum);
+	//on load, random number assigned from 1-12 to each crystal
+	//for the duration of the game
+		var green = Number(1 + Math.floor(Math.random() * 12));
+        document.getElementById("green").value = green;
+        console.log(Number($('#green').val()));
 
+		do {
+			var red = 1 + Math.floor(Math.random() * 12);
+		} while (red === green);
+        document.getElementById("red").value = red;
+        console.log(Number($('#red').val()));
+
+		do {
+			var blue = 1 + Math.floor(Math.random() * 12);
+		} while (blue === red || blue === green);		
+        document.getElementById("blue").value = blue;
+        console.log(Number($('#blue').val()));
+
+        do{
+			var yellow = 1 + Math.floor(Math.random() * 12);
+		} while (yellow === green || yellow === red 
+			|| yellow === blue);
+        document.getElementById("yellow").value = yellow;
+        console.log(Number($('#yellow').val()));
+	//==========================================================
+        test=false;
+        newGame();
+}
 function newGame()
 {
 	//RESET
   	//=====================================================
   		ranNum = 0;
- 		score = 0;
+ 		score = '';
  		green = 0;
  		red = 0;
  		blue = 0;
@@ -55,39 +96,24 @@ function newGame()
         document.getElementById("yellow").value = yellow;
         console.log(Number($('#yellow').val()));
 }
-
-$(document).ready(function() 
+function winLose()
 {
-	// on load, random number generated between 19-120
-
-    	var ranNum = 19 + Math.floor(Math.random() * 100);
-        $('#ranNum').text(ranNum);
-        console.log(ranNum);
-
-	//on load, random number assigned from 1-12 to each crystal
-	//for the duration of the game
-		var green = Number(1 + Math.floor(Math.random() * 12));
-        document.getElementById("green").value = green;
-        console.log(Number($('#green').val()));
-
-		do {
-			var red = 1 + Math.floor(Math.random() * 12);
-		} while (red === green);
-        document.getElementById("red").value = red;
-        console.log(Number($('#red').val()));
-
-		do {
-			var blue = 1 + Math.floor(Math.random() * 12);
-		} while (blue === red || blue === green);		
-        document.getElementById("blue").value = blue;
-        console.log(Number($('#blue').val()));
-
-        do{
-			var yellow = 1 + Math.floor(Math.random() * 12);
-		} while (yellow === green || yellow === red 
-			|| yellow === blue);
-        document.getElementById("yellow").value = yellow;
-        console.log(Number($('#yellow').val()));
+	if (score === ranNum) 
+	{
+		wins++;
+		document.getElementById('wins').innerHTML = wins;
+		reset();
+	}
+	else if (score > ranNum)
+	{
+		losses++;
+		document.getElementById('losses').innerHTML = losses;
+		reset();
+	}	
+}
+//MAIN PROCESS
+//=======================================================================
+	newGame();
 
 	//on click, crystals will add unknown assigned No. to score
 
@@ -101,14 +127,5 @@ $(document).ready(function()
 			{	
 				wins = wins + 1;
 				$('#wins').text(wins);
-				newGame();
 			}
-			else if (score > ranNum)
-			{
-				losses = losses + 1;
-				$('#losses').text(losses);
-				newGame();
-			}
-
 		});
-});
