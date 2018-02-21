ad6d9a0cbf0119765dfa6928be3743a6bbb4b9bc
Melvin Vasquez <mxv647@med.miami.edu>
2018-02-20 19:55:28 -0500

    JS fixed, to-do CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 //GLOBAL VARIABLES
 //=====================================================
 
-var random = 0;
+var ranNum = 0;
 var wins = 0;
 var losses = 0;
 var score = 0;
@@ -17,84 +17,53 @@ function newGame()
 {
 	//RESET
   	//=====================================================
-  		ranNum = 0;
- 		score = 0;
- 		green = 0;
- 		red = 0;
- 		blue = 0;
- 		yellow = 0;
 
 	// on load, random number generated between 19-120
 
-    	var ranNum = 19 + Math.floor(Math.random() * 100);
+    	ranNum = 19 + Math.floor(Math.random() * 101);
         $('#ranNum').text(ranNum);
         console.log(ranNum);
 
 	//on load, random number assigned from 1-12 to each crystal
 	//for the duration of the game
-		var green = Number(1 + Math.floor(Math.random() * 12));
+		green = Number(1 + Math.floor(Math.random() * 11));
         document.getElementById("green").value = green;
         console.log(Number($('#green').val()));
 
 		do {
-			var red = 1 + Math.floor(Math.random() * 12);
+			red = 1 + Math.floor(Math.random() * 11);
 		} while (red === green);
         document.getElementById("red").value = red;
         console.log(Number($('#red').val()));
 
 		do {
-			var blue = 1 + Math.floor(Math.random() * 12);
+			blue = 1 + Math.floor(Math.random() * 11);
 		} while (blue === red || blue === green);		
         document.getElementById("blue").value = blue;
         console.log(Number($('#blue').val()));
 
         do{
-			var yellow = 1 + Math.floor(Math.random() * 12);
+			yellow = 1 + Math.floor(Math.random() * 11);
 		} while (yellow === green || yellow === red 
 			|| yellow === blue);
         document.getElementById("yellow").value = yellow;
         console.log(Number($('#yellow').val()));
-}
 
+        score = 0;
+        $('#score').text(score);
+}
+//MAIN PROCESS
+//============================================================
 $(document).ready(function() 
 {
-	// on load, random number generated between 19-120
-
-    	var ranNum = 19 + Math.floor(Math.random() * 100);
-        $('#ranNum').text(ranNum);
-        console.log(ranNum);
-
-	//on load, random number assigned from 1-12 to each crystal
-	//for the duration of the game
-		var green = Number(1 + Math.floor(Math.random() * 12));
-        document.getElementById("green").value = green;
-        console.log(Number($('#green').val()));
-
-		do {
-			var red = 1 + Math.floor(Math.random() * 12);
-		} while (red === green);
-        document.getElementById("red").value = red;
-        console.log(Number($('#red').val()));
-
-		do {
-			var blue = 1 + Math.floor(Math.random() * 12);
-		} while (blue === red || blue === green);		
-        document.getElementById("blue").value = blue;
-        console.log(Number($('#blue').val()));
-
-        do{
-			var yellow = 1 + Math.floor(Math.random() * 12);
-		} while (yellow === green || yellow === red 
-			|| yellow === blue);
-        document.getElementById("yellow").value = yellow;
-        console.log(Number($('#yellow').val()));
+	newGame();
 
 	//on click, crystals will add unknown assigned No. to score
 
 		$('.crystal').on('click', function() 
 		{
-			var onClick = Number($(this).val());
-			score = (score * 1) + (onClick * 1);
+			var onClick = parseInt($(this).val());
+			score = score + onClick;
 			$('#score').text(score);
 			console.log(score);
 			if (score === ranNum) 
